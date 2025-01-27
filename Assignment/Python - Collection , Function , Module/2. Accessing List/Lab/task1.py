98721507c764d8185f6ef208777a53f598f0722c
Sumit Yadav <brijbinisy@gmail.com>
2025-01-27 09:21:39 +0530

    Add files via upload

---
+++ Assignment/Python - Collection , Function , Module/2. Accessing List/Lab/task1.py
@@ -0,0 +1,10 @@
+my_list = [1,3,5,2,7,8,4,9,"Sumit","Hello World", True , False , 'c']
+
+# Omit start: Slice from the beginning
+print(my_list[:4]) 
+
+# Omit stop: Slice until the end
+print(my_list[6:])  
+
+# Omit both: Create a copy of the list
+print(my_list[:]) 
