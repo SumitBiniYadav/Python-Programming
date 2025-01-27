8bed0efe2539b5b95ac5e57183d463c1a0884f2a
Sumit Yadav <brijbinisy@gmail.com>
2025-01-27 09:18:17 +0530

    Add files via upload

---
+++ Assignment/Python - Collection , Function , Module/1. List Operations/Practicle/Task1.py
@@ -0,0 +1,13 @@
+# Initial list
+my_list = ["apple", "banana", "cherry", "Tuesday", 2,4,6,True,False]
+print("Initial list:", my_list)
+
+    # Adding elements using append()
+my_list.append("orange")
+print("After appending 'orange':", my_list)
+
+# Adding elements using insert()
+my_list.insert(1, "blueberry")  # Insert 'blueberry' at index 1
+print("After inserting 'blueberry' at index 1:", my_list)
+
+  
