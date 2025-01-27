3b5c2f7036a320d8642bb62b0fc100921cf95730
Sumit Yadav <brijbinisy@gmail.com>
2025-01-27 10:18:36 +0530

    Add files via upload

---
+++ Assignment/Python - Collection , Function , Module/4. Tuple/Lab/task.2py.py
@@ -0,0 +1,9 @@
+# Define two tuples
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+
+# Concatenate the tuples
+result = tuple1 + tuple2
+
+# Print the concatenated tuple
+print(result)
---
+++ Assignment/Python - Collection , Function , Module/4. Tuple/Lab/task1.py
@@ -0,0 +1,5 @@
+# Create a tuple with multiple data types
+my_tuple = (42, "Hello", 3.14, True, ["apple", "banana"], (1, 2))
+
+# Print the tuple
+print(my_tuple)
