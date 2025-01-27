83e34b0319b35899c93109a7bcf957d6863362a0
Sumit Yadav <brijbinisy@gmail.com>
2025-01-27 09:17:20 +0530

    Add files via upload

---
+++ Assignment/Python - Collection , Function , Module/1. List Operations/Lab/task1.py
@@ -0,0 +1,11 @@
+my_list = ["apple", "banana", "cherry"]
+print("Initial list:", my_list)
+
+# Adding elements using append()
+my_list.append("orange")
+print("After appending 'orange':", my_list)
+
+# Adding elements using insert()
+my_list.insert(1, "blueberry")  # Insert 'blueberry' at index 1
+print("After inserting 'blueberry' at index 1:", my_list)
+
---
+++ Assignment/Python - Collection , Function , Module/1. List Operations/Lab/task2.py
@@ -0,0 +1,11 @@
+my_list = ["apple", "banana", "cherry"]
+print("Initial list:", my_list)
+
+
+# Removing elements using pop()
+removed_element = my_list.pop()  # Removes the last element
+print(f"After popping the last element ('{removed_element}'):", my_list)
+
+# Removing elements using remove()
+my_list.remove("banana")  # Removes 'banana' from the list
+print("After removing 'banana':", my_list)
