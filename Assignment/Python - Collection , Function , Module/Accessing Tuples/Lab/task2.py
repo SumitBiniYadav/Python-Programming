f1c7f7d3b1c43f8b4e2e9292cd166f224ba4182a
Sumit Yadav <brijbinisy@gmail.com>
2025-02-08 22:52:39 +0530

    Delete Assignment/Python - Collection , Function , Module/Accessing Tuples directory

--- Assignment/Python - Collection , Function , Module/Accessing Tuples/Lab/task2.py
+++
@@ -1,3 +0,0 @@
-t = ('Apple','Banana','Mango','Pineapple','Cherry','Kiwi')
-print(len(t))
-print("Last Index Value Is : ",t[5])
--- Assignment/Python - Collection , Function , Module/Accessing Tuples/Practicle/task2.py
+++
@@ -1,3 +0,0 @@
-t = ('Apple','Banana','Mango','Pineapple','Cherry','Kiwi')
-
-print("Tuple Index Value Between 1 to 5 Is : ",t[1:6])
