883cbbba9d6a81336e7ac06a53a1471fb7a3624c
Sumit Yadav <brijbinisy@gmail.com>
2025-02-16 14:44:05 +0530

    Add files via upload

---
+++ Assignment/Advance Python Programming/3. Opening and Closing Files/Lab/task1.py
@@ -0,0 +1,11 @@
+# Open the file in write mode
+file = open("task1.txt", "w")
+
+# Write some text to the file
+file.write("Hello, this is a test file.\n")
+file.write("Writing some text into the file using Python.\n")
+
+# Close the file
+file.close()
+
+print("File has been written and closed successfully.")
