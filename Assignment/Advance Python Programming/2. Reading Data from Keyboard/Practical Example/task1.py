852f3009812b31ae48edf90c382e644188a0465b
Sumit Yadav <brijbinisy@gmail.com>
2025-02-16 14:42:01 +0530

    Add files via upload

---
+++ Assignment/Advance Python Programming/2. Reading Data from Keyboard/Practical Example/task1.py
@@ -0,0 +1,10 @@
+# Read user input
+string_value = input("Enter a string: ")
+integer_value = int(input("Enter an integer: "))
+float_value = float(input("Enter a float: "))
+
+# Display the values
+print("\nYou entered:")
+print(f"String: {string_value}")
+print(f"Integer: {integer_value}")
+print(f"Float: {float_value}")
