6f142d1bb5464c54e5d7ee5ca677891d9ac91902
Sumit Yadav <brijbinisy@gmail.com>
2025-02-16 14:40:35 +0530

    Add files via upload

---
+++ Assignment/Advance Python Programming/2. Reading Data from Keyboard/Lab/task1.py
@@ -0,0 +1,7 @@
+# Read user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Print formatted output
+print(f"Hello, {name}! You are {age} years old.")
+print("Next year, you will be {} years old.".format(age + 1))
