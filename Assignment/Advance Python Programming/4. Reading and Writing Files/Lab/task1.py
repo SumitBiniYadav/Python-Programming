22b9ddc8fe13d313e16c12dd9b9e1f35efa4d036
Sumit Yadav <brijbinisy@gmail.com>
2025-02-19 20:39:22 +0530

    Add files via upload

---
+++ Assignment/Advance Python Programming/4. Reading and Writing Files/Lab/task1.py
@@ -0,0 +1,7 @@
+# Open the file in read mode
+with open("sample.txt", "r") as file:
+    # Read the contents of the file
+    contents = file.read()
+    
+    # Print the contents
+    print(contents)
---
+++ Assignment/Advance Python Programming/4. Reading and Writing Files/Practical Examples/task2.py
@@ -0,0 +1,7 @@
+# Open the file in read mode
+with open("input.txt", "r") as file:
+    # Read the file content
+    data = file.read()
+    
+    # Print the content
+    print(data)
